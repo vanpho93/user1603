4b9db2ea78061b73e2349df167fb821a4550b03f
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-15 20:12:11 +0700

    xong ui

--- db.js
+++ db.js
@@ -10,14 +10,22 @@ const config = {
 
 const pool = new pg.Pool(config);
 
-
 function query(sql, cb) {
     pool.connect((err, client, done) => {
         if (err) return cb(err);
         client.query(sql, (errQuery, result) => {
+            done(errQuery);
             if (errQuery) return cb(errQuery);
             cb(undefined, result);
         });
     });
 }
 
+//function insert new user
+
+const insertUser = (username, password, email, name, cb) => {
+    const sql = '';
+    
+};
+
+//fucntion check login
--- index.js
+++ index.js
@@ -7,3 +7,7 @@ app.use(express.static('public'));
 app.listen(3000, () => console.log('Server started'));
 
 app.get('/', (req, res) => res.render('home'));
+
+app.get('/signup', (req, res) => res.render('signup'));
+
+app.get('/signin', (req, res) => res.render('signin'));
